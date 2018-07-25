b66ef7a4c9c9600f29926c3265f16b2110a49da7
unknown <katiajaafar28@gmail.com>
2018-07-25 13:34:06 +0300

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import "./Styles/styles.scss";
 import 'react-dates/lib/css/_datepicker.css'; 
 
 const store = configureStore();
-
+console.log('testing');
 //const state = store.getState();
 //const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
